cbf1ffac0c608abd94e304bfab4a0b24534afe5a
jquinn89 <43919006+jquinn89@users.noreply.github.com>
2019-05-12 09:55:22 -0400

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var fs = require('fs');
+
+console.log("Server starting at localhost 3000");
+http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type': 'video/mp4'});
+    var rs = fs.createReadStream("hay.mp4");
+    rs.pipe(response);
+
+}).listen(8080);
